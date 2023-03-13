3d2a3a6f090950b132fa49badea0a8e0e9d3fc13
Falcon <falcon@zigtag.dev>
2023-03-13 15:26:40 -0700

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# EDDTeam15Code
+
+This is our code repository for our team's mechanism.
+
+## Dependencies
+- PlatformIO
---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include<Arduino.h>
+#include <Servo.h>
+
+const int button1Pin = 2;
+const int button2Pin = 3;
+int button1State = 0;
+int button2State = 0;
+
+const int leftMotorPin = 9;
+Servo leftMotor;
+
+void motorControl(Servo motor, int value);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Started Serial Port");
+
+  pinMode(button1Pin, INPUT);
+
+  pinMode (leftMotorPin, OUTPUT);
+  leftMotor.attach(leftMotorPin);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  button1State = digitalRead(button1Pin);
+  button2State = digitalRead(button2Pin);
+
+  // Serial.println(buttonState);
+
+  if (button1State == HIGH) {
+    motorControl(leftMotor, 100);  
+  } else if (button2State == HIGH) {
+    motorControl(leftMotor, -100);
+  } else {
+    motorControl(leftMotor, 0);
+  }
+
+  delay(50);
+}
+
+void motorControl(Servo motor, int value) {
+  motor.write(map(value, -255, 255, 1000, 2000));
+}
